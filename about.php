35bf555ee5c5525554d22e33be4e1f046975d7cd
Sarthak Pradhan <pradhansarthak2016@gmail.com>
2024-08-29 23:47:44 +1000

    about page updated

--- about.php
+++ about.php
@@ -3,9 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>PHP Version and Task Overview</title>
 </head>
 <body>
+<div class="center-container">
+
+<!-- Navigation Bar -->
+<div class="navbar">
+    
+    <div class="menu">
+        <a href="index.php">Home</a>
+        <a href="postjobform.php">Post Job</a>
+        <a href="searchjobform.php">Search Job</a>
+    </div>
+</div>
+
+</div> 
+
+<div class="form-container">
     <h1>Project Overview</h1>
     <ul>
         <li><strong>PHP Version Installed on Mercury:</strong> 
@@ -13,20 +29,20 @@
         </li>
         <li><strong>Tasks Not Attempted or Not Completed:</strong>
             <ul>
-                <li>All completed</li>
+                All completed till task 8
                
             </ul>
         </li><br>
         <li><strong>Special Features Implemented or Attempted:</strong>
             <ul>
-                <li>Special features</li>
+                Special features
                 
             </ul>
         </li><br>
         <li><strong>What discussion points did you participated on in the unit’s discussion board for
         Assignment 1? If you did not participate, state your reason.</strong>
             <ul>
-                <li>I did not use the discussion board because I had no problem with anything.</li>
+                I did not use the discussion board because I had no problem with anything.
                 
             </ul>
         </li>
@@ -34,5 +50,6 @@
 
     <br><a href='index.php'>Home page</a> | <a href='searchjobform.php'>Search Job Vacancy page</a>
 
+</div>
 </body>
 </html>
